0e0051490fdf1bcd4da259c9f75f2cba5ee78e45
SimonVanBerkum <simonvbps4@gmail.com>
2022-04-01 10:06:10 +0200

    Menu burger index fini - Simon

--- boilerplate/burger.js
+++ boilerplate/burger.js
@@ -1,20 +1,23 @@
 let link = document.getElementById('link');
 let burger = document.getElementById('burger');
-let logo = document.querySelector('logo');
+let nav = document.querySelector('nav')
+let logo = document.querySelector('img.logo');
 let header = document.querySelector('header');
-let ul = document.querySelector('ul');
-let mapnav = document.querySelector('.mapgoogle');
-let ulreseaux = document.querySelector('.reseaux');
+let ul = document.querySelector('ul.navul');
+let mapnav = document.querySelector('div.mapgoogle');
+let ulreseaux = document.querySelector('ul.reseaux');
 let footer = document.querySelector('footer');
+let container = document.querySelector('main.container');
 
 link.addEventListener('click', function(e) {
   e.preventDefault()
   burger.classList.toggle('open');
   logo.classList.toggle('open');
+  nav.classList.toggle('open')
   header.classList.toggle('open');
   ul.classList.toggle('open');
   mapnav.classList.toggle('open');
   ulreseaux.classList.toggle('open');
   footer.classList.toggle('open');
-  
+  container.classList.toggle('open');
 })
